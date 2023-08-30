cce9dd68efe983a97d50376a024ea14d3afa9bc6
M <amfrombr@gmail.com>
2023-08-30 14:01:10 -0400

    changes to settings so things work

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import http from 'http';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
